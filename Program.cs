c18d75f671a669131096bc3c1b4b3a534744b534
dimon12091 <dmytro12091@gmail.com>
2021-03-30 11:15:11 +0300

    commited Program.cs

--- Program.cs
+++ Program.cs
@@ -9,13 +9,18 @@ using System.Threading.Tasks;
 
 namespace vocabulary_app
 {
+    //The host is typically configured, built, and run by code in the Program class.
     public class Program
     {
+        //The Main method:
+        //Calls a CreateHostBuilder method to create and configure a builder object.
+        //Calls Build and Run methods on the builder object.
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
         }
 
+        // For an HTTP workload , web templates generate the following code to create a host
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
